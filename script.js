17a715d6c747a70c63d3de39864f6d7702c26ffe
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-03-16 01:21:31 -0700

    log words

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('hiii')
+const text = "hello world this is a speed reader test"
+
+const delay = (time) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, time)
+    })
+}
+
+const displayText = async (text) => {
+    const arr = text.split(" "), n = arr.length
+    let i = 0
+    while (i < n) {
+        console.log(arr[i++])
+        await delay(1000)
+    }
+
+}
+
+displayText(text)
