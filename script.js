7b61548c690598062c0d9aec7fe918245a6d7eee
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-03-16 02:15:53 -0700

    add pause & resume

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 const textInput = document.querySelector("#text-input"), readerButton = document.querySelector("#reader-btn")
+let i = 0, isRunning = false, interval = null, textArray
+const INTERVAL_TIME = 500
 
 const delay = (time) => {
     return new Promise((resolve, reject) => {
-        setTimeout(resolve, time)
+        setTimeout(resolve, INTERVAL_TIME)
     })
 }
 
 const displayText = async () => {
-    const text = textInput.value
-    const arr = text.split(/\s+/), n = arr.length
-    let i = 0
-    while (i < n) {
-        console.log(arr[i++])
-        await delay(1000)
+    console.log(textArray[i++])
+    if (i === textArray.length) {
+        clearInterval(interval)
+        isRunning = false
+        i = 0;
+        readerButton.innerHTML = "Start"
     }
-
 }
 
-readerButton.addEventListener("click", displayText)
+const runHandler = () => {
+    if (isRunning) {
+        isRunning = false
+        clearInterval(interval)
+        readerButton.innerHTML = "Start"
+    }
+    else {
+        isRunning = true
+        interval = setInterval(displayText, INTERVAL_TIME)
+        readerButton.innerHTML = "Pause"
+    }
+}
+
+textInput.addEventListener("input", e => {
+    i = 0;
+    isRunning = false;
+    clearInterval(interval)
+    const text = textInput.value
+    textArray = text.split(/\s+/)
+})
+
+readerButton.addEventListener("click", runHandler)
